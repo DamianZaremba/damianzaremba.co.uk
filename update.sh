b4ebb9310cacf16ff341ecc4fe9588bdcc6c76c6
Damian Zaremba <damian@damianzaremba.co.uk>
2013-01-19 16:02:16 +0000

    Adding update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+test -f ~/.running && exit 0
+cd ~/damianzaremba.co.uk
+PRE_HASH=$(git rev-parse HEAD)
+git pull origin master
+POST_HASH=$(git rev-parse HEAD)
+
+if [ "$PRE_HASH" != "$POST_HASH" ];
+then
+	make install
+fi
+rm -f ~/.running
