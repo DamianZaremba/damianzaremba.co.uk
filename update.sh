f27c453a3b3b2c3e0a24321b4d9f223a9e78ebf0
Damian Zaremba <damian@damianzaremba.co.uk>
2013-01-19 16:41:06 +0000

    Enable lsi

--- update.sh
+++ update.sh
@@ -2,12 +2,15 @@
 # This is suppose to run as a cronjob and enforce locking
 test -f ~/.running && exit 0
 cd ~/damianzaremba.co.uk
+git checkout -- _config.yml
+
 PRE_HASH=$(git rev-parse HEAD)
 git pull origin master
 POST_HASH=$(git rev-parse HEAD)
 
 if [ "$PRE_HASH" != "$POST_HASH" ];
 then
+	sed -i 's/lsi:\s*false/lsi: true/' _config.yml
 	make install
 fi
 rm -f ~/.running
